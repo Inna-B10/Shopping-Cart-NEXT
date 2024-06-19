00d487bca91c34c1fe87d4731ae8e5dbc2004f43
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-06-19 10:46:30 +0200

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Shopping_Cart_NEXT
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
