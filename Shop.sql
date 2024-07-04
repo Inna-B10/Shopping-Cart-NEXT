9ae852f78865045527be4016981820d2ecf4b73e
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-07-04 13:52:31 +0200

    clean up DB

--- Shop.sql
+++ Shop.sql
@@ -45,7 +45,7 @@
 --    TABLE_NAME = 'Categories';
 
 
-
+--Create table Categories
 --CREATE TABLE Categories (
 --    cat_id INT IDENTITY(1,1),
 --    cat_name VARCHAR(255) NOT NULL,
@@ -55,25 +55,27 @@
 --ALTER TABLE Categories
 --ADD CONSTRAINT UQ_cat_name UNIQUE (cat_name);
 
+--insert categories name
 --insert into Categories (cat_name)
 --values ('Earrings'), ('Necklaces'),('Bracelets'),('Anklets'),('Brooches'),('Pendants'),('Sets')
 
 
-CREATE TABLE Products (
-    prod_id INT IDENTITY(1,1),
-    prod_name VARCHAR(255) NOT NULL,
-    cat_id INT NOT NULL,
-    prod_price DECIMAL(18,2) NOT NULL,
-    prod_price_discounted DECIMAL(18,2) NULL,
-    prod_desc_short VARCHAR(255) NULL,
-    prod_desc_full VARCHAR(MAX) NULL,
-    prod_article_num VARCHAR(50) NULL,
-    prod_tags VARCHAR(255) NULL,
-    prod_is_stone BIT NOT NULL,
-    prod_label VARCHAR(100) NULL,
-    CONSTRAINT PK_prod_id PRIMARY KEY (prod_id),
-    CONSTRAINT FK_prod_cat_id FOREIGN KEY (cat_id) REFERENCES Categories(cat_id)
-);
+--create table Products
+--CREATE TABLE Products (
+--    prod_id INT IDENTITY(1,1),
+--    prod_name VARCHAR(255) NOT NULL,
+--    cat_id INT NOT NULL,
+--    prod_price DECIMAL(18,2) NOT NULL,
+--    prod_price_discounted DECIMAL(18,2) NULL,
+--    prod_desc_short VARCHAR(255) NULL,
+--    prod_desc_full VARCHAR(MAX) NULL,
+--    prod_article_num VARCHAR(50) NULL,
+--    prod_tags VARCHAR(255) NULL,
+--    prod_is_stone BIT NOT NULL,
+--    prod_label VARCHAR(100) NULL,
+--    CONSTRAINT PK_prod_id PRIMARY KEY (prod_id),
+--    CONSTRAINT FK_prod_cat_id FOREIGN KEY (cat_id) REFERENCES Categories(cat_id)
+--);
 
 -- Индексирование
 --CREATE INDEX idx_prod_name ON Products(prod_name);
@@ -95,54 +97,44 @@ CREATE TABLE Products (
 --ALTER TABLE Products
 --ADD CONSTRAINT CK_prod_price_discounted CHECK (prod_price_discounted >= 0);
 
---составление меню категорий
---select cat_name from Categories
---union all
---select prod_label from Products
+--insert products
+--insert into Products(prod_name, cat_id,prod_price,prod_price_discounted,prod_desc_short,prod_desc_full,prod_article_num,prod_tags,prod_is_stone,prod_label)
+--values('Clara', 1,4998,4248,'Diamantring i 375 hvitt gull 0,11 ct',
+--'Clara diamantring i 375 hvitt gull med 50 skinnende diamanter i ulike størrelser som samlet er 0,11 ct W. Dette er en tidløs ring som er fin alene men også i kombinasjon med flere smykker i hvitt gull. Dekorasjonen på ringen er 7 mm i diameter og skaper et flott blikkfang.',
+--'59079', 'white gold', 1,'15%')
+
+--insert into Products(prod_name, cat_id,prod_price,prod_desc_short,prod_desc_full,prod_article_num,prod_tags,prod_is_stone,prod_label)
+--values('Nora', 1,3499,'Diamantring i 375 gult gull 0,09 ct',
+--'Klassisk diamantring i 375 (9 karat) gult gull med tre skinnende diamanter på totalt 0,09 ct. To av diamantene er 0,025 ct og den midterste diamanten er 0,04 ct. Diamantene har 16/16 slip med brilliant form som betyr at det er 16 fasetter på overdelen og 16 fasetter på underdelen. Ringen finnes også i hvitt gull og er en av våre bestselgere blant konfirmasjonsgaver til henne. Dette er en tidløs og vakker ring. Ringen er 2 mm bred og er fin i kombinasjon med flere ringer.',
+--'56471', 'gold', 1,'new')
+
 
 use ShopDb
 
-insert into Products(prod_name, cat_id,prod_price,prod_price_discounted,prod_desc_short,prod_desc_full,prod_article_num,prod_tags,prod_is_stone,prod_label)
-values('Clara', 1,4998,4248,'Diamantring i 375 hvitt gull 0,11 ct',
-'Clara diamantring i 375 hvitt gull med 50 skinnende diamanter i ulike størrelser som samlet er 0,11 ct W. Dette er en tidløs ring som er fin alene men også i kombinasjon med flere smykker i hvitt gull. Dekorasjonen på ringen er 7 mm i diameter og skaper et flott blikkfang.',
-'59079', 'white gold', 1,'15%')
+--test to get categories+labels
+select cat_name from Categories
+union all
+select prod_label from Products
 
-insert into Products(prod_name, cat_id,prod_price,prod_desc_short,prod_desc_full,prod_article_num,prod_tags,prod_is_stone,prod_label)
-values('Nora', 1,3499,'Diamantring i 375 gult gull 0,09 ct',
-'Klassisk diamantring i 375 (9 karat) gult gull med tre skinnende diamanter på totalt 0,09 ct. To av diamantene er 0,025 ct og den midterste diamanten er 0,04 ct. Diamantene har 16/16 slip med brilliant form som betyr at det er 16 fasetter på overdelen og 16 fasetter på underdelen. Ringen finnes også i hvitt gull og er en av våre bestselgere blant konfirmasjonsgaver til henne. Dette er en tidløs og vakker ring. Ringen er 2 mm bred og er fin i kombinasjon med flere ringer.',
-'56471', 'gold', 1,'new')
-
-insert into Products(prod_name, cat_id,prod_price,prod_desc_short,prod_desc_full,prod_article_num,prod_tags,prod_is_stone,prod_label)
-values('Nora', 1,3499,'Diamantring i 375 gult gull 0,09 ct',
-'Klassisk diamantring i 375 (9 karat) gult gull med tre skinnende diamanter på totalt 0,09 ct. To av diamantene er 0,025 ct og den midterste diamanten er 0,04 ct. Diamantene har 16/16 slip med brilliant form som betyr at det er 16 fasetter på overdelen og 16 fasetter på underdelen. Ringen finnes også i hvitt gull og er en av våre bestselgere blant konfirmasjonsgaver til henne. Dette er en tidløs og vakker ring. Ringen er 2 mm bred og er fin i kombinasjon med flere ringer.',
-'56471', 'gold', 1,'new')
-
+--test to get products by category/label
 SELECT p.*, c.cat_name 
 FROM Products p
 INNER JOIN 
 Categories c ON p.cat_id = c.cat_id
-WHERE p.cat_id = 1;
+WHERE c.cat_name = 'new' or p.prod_label = 'new';
+
+--test to get items in ShoppingCart
+--select P.ID, P.Name, P.Image, P.ActualPrice, P.DiscountedPrice from ShoppingCart C
+--INNER JOIN Images P
+--ON C.ProductID = P.Id;
 
 
 
-
-
-
-
-
-
-
-
-select P.ID, P.Name, P.Image, P.ActualPrice, P.DiscountedPrice from ShoppingCart C
-INNER JOIN Images P
-ON C.ProductID = P.Id;
 
 --delete from ShoppingCart where ProductID = 6
 
 --ALTER TABLE Products
 --DROP COLUMN prod_size;
-
-
 
 select * from Products
 select * from Categories
