33a6f1c2c890f5f85aee03eef61e5dd4c7e18c78
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-07-21 04:12:01 +0200

    some styles and text for subCat and discount. start design triple category

--- wwwroot/app/page.jsx
+++ wwwroot/app/page.jsx
@@ -26,11 +26,15 @@ export default function Home() {
 				<div className={styles.ctgUpper}>
 					<h2 className={`${styles.ctgTitle} ${cinzel.variable}`}>Rings</h2>
 					<p className={styles.ctgDesc}>
-						When choosing between gold and silver rings, it is important to
-						consider the benefits of each type. Gold rings are known for their
-						timeless elegance and durability, while silver rings are valued for
-						their affordability and versatility. Both options have their unique
-						appeal, so it ultimately depends on your personal style and budget.
+						When choosing rings, it is essential to consider the distinct
+						qualities of gold and silver. Gold rings are known for their
+						timeless elegance, durability, and rich hues, making them a
+						sophisticated choice suitable for any occasion and a lasting
+						investment in style. Silver rings, with their sleek and modern
+						design, offer a contemporary appeal for a casual yet chic look,
+						perfect for both daily wear and special occasions. At LuxGleam, we
+						offer an exquisite selection of both, ensuring you find the perfect
+						ring to suit your individual style.
 					</p>
 				</div>
 				<div className={`${styles.ctgBottom} flex`}>
@@ -41,13 +45,13 @@ export default function Home() {
 					/>
 					<SubCatCart
 						src={'/media/1/gold.png'}
-						cat_name='Gold Rings'
+						cat_name='Gold rings'
 						color='gold'
 					/>
 				</div>
 			</section>
 			<section className={`${styles.discountWrap} flex`}>
-				<div className={styles.discLeft}>
+				<div className={`${styles.discLeft} ${styles.disc15}`}>
 					<div className={`${styles.discPrice} ${messiri.variable} flex`}>
 						-15%
 					</div>
@@ -55,32 +59,161 @@ export default function Home() {
 				<div className={`${styles.discRight} flex column`}>
 					<h3 className={styles.discTitle}>Sale</h3>
 					<p className={styles.discDesc}>
-						Please take the time to browse through our selection of high-quality
-						jewelry, now available at a considerable 15% discount.
+						{/* Please take the time to browse through our selection of high-quality
+						jewelry, now available at a considerable 15% discount. */}
+						Jewelry has the power to transform any outfit, and at LuxGleam, we
+						believe that every piece tells a story. Our collection features
+						stunning bijouterie that cater to diverse tastes and styles,
+						ensuring you find the perfect accessory to express yourself.
+						<br />
+						<br />
+						Explore our selection of products with a considerable 15% discount.
 					</p>
 				</div>
 			</section>
-			<section id='earrings'>
-				<h2>Earrings</h2>
+			<section id='earrings' className={`${styles.ctgWrap} flex column`}>
+				<div className={styles.ctgUpper}>
+					<h2 className={`${styles.ctgTitle} ${cinzel.variable}`}>Earrings</h2>
+					<p className={styles.ctgDesc}>
+						Earrings are a perfect way to express your individuality and add a
+						touch of elegance to any outfit. Whether you prefer the classic glam
+						of gold or the trendy allure of silver, LuxGleam offers a variety of
+						designs to suit every taste. From delicate studs to statement hoops,
+						our earrings are crafted to enhance your natural beauty and reflect
+						your personal style.
+					</p>
+				</div>
+				<div className={`${styles.ctgBottom} flex`}>
+					<SubCatCart
+						src={'/media/2/silver.png'}
+						cat_name='Silver earrings'
+						color='silver'
+					/>
+					<SubCatCart
+						src={'/media/2/gold.png'}
+						cat_name='Gold earrings'
+						color='gold'
+					/>
+				</div>
 			</section>
 			<section className={styles.galleryWrap}>
 				<h2 className={styles.galleryTitle}>Trendy</h2>
 				<div className={styles.gallery}>Gallery</div>
 			</section>
-			<section id='bracelets'>
-				<h2>Bracelets</h2>
+			<section id='bracelets' className={`${styles.ctgWrap} flex column`}>
+				<div className={styles.ctgUpper}>
+					<h2 className={`${styles.ctgTitle} ${cinzel.variable}`}>Bracelets</h2>
+					<p className={styles.ctgDesc}>
+						Bracelets are more than just accessories; they are versatile pieces
+						that can elevate any look. At LuxGleam, our stunning array of gold
+						and silver bracelets allows you to make a unique statement. Gold
+						bracelets exude luxury and timelessness, making them perfect for
+						formal occasions or adding a touch of elegance to everyday wear. On
+						the other hand, silver bracelets offer a contemporary vibe and
+						versatility, seamlessly fitting into both casual and chic ensembles.
+						Explore our collection to discover the bracelet that perfectly
+						complements your unique style.
+					</p>
+				</div>
+				<div className={`${styles.ctgBottom} flex`}>
+					<SubCatCart
+						src={'/media/3/silver.png'}
+						cat_name='Silver bracelets'
+						color='silver'
+					/>
+					<SubCatCart
+						src={'/media/3/gold.png'}
+						cat_name='Gold bracelets'
+						color='gold'
+					/>
+				</div>
 			</section>
-			<section id='discount'>
-				<div>sale 25%</div>
+			<section className={`${styles.discountWrap} flex`}>
+				<div className={`${styles.discLeft} ${styles.disc25}`}>
+					<div className={`${styles.discPrice} ${messiri.variable} flex`}>
+						-25%
+					</div>
+				</div>
+				<div className={`${styles.discRight} flex column`}>
+					<h3 className={styles.discTitle}>Sale</h3>
+					<p className={styles.discDesc}>
+						{/* Check out our amazing collection of gorgeous jewelry and get a sweet deal of 25% off! */}
+						At LuxGleam, we understand that jewelry is not just a decoration; it
+						is an extension of who you are. Our carefully curated collection of
+						accessories ensures that you can find pieces that resonate with your
+						individuality.
+						<br />
+						<br />
+						Check out our amazing assortment of gorgeous jewelry and get a sweet
+						deal of 25% off!
+					</p>
+				</div>
 			</section>
-			<section id='necklaces'>
-				<h2>Necklaces</h2>
+			<section id='necklaces' className={`${styles.ctgWrap} flex column`}>
+				<div className={styles.ctgUpper}>
+					<h2 className={`${styles.ctgTitle} ${cinzel.variable}`}>Necklaces</h2>
+					<p className={styles.ctgDesc}>
+						A necklace has the power to transform an outfit and become a
+						cherished piece in your jewelry collection. LuxGleam’s selection of
+						gold and silver necklaces includes everything from elegant pendants
+						and sophisticated chains to eye-catching statement pieces. Whether
+						you’re drawn to the classic beauty of gold or the sleek
+						sophistication of silver, our necklaces are designed to reflect your
+						unique style and personality.
+					</p>
+				</div>
+				<div className={`${styles.ctgBottom} flex`}>
+					<SubCatCart
+						src={'/media/4/silver.png'}
+						cat_name='Silver necklaces'
+						color='silver'
+					/>
+					<SubCatCart
+						src={'/media/4/gold.png'}
+						cat_name='Gold necklaces'
+						color='gold'
+					/>
+				</div>
 			</section>
-			<section id='discount'>
-				<div>Outlet</div>
+			<section className={styles.galleryWrap}>
+				<h2 className={styles.galleryTitle}>Outlet</h2>
+				<div className={styles.gallery}>Gallery</div>
 			</section>
-			<section id='sets'>
-				<h2>Brooches, pendants, anklets</h2>
+			<section id='sets' className={`${styles.ctgWrap} flex column`}>
+				<div className={styles.ctgUpper}>
+					<h2 className={`${styles.ctgTitle} ${cinzel.variable}`}>
+						Brooches, pendants, anklets
+					</h2>
+				</div>
+				<div className='flex'>
+					<div className='flex column'>
+						<p className={styles.ctgDesc}>
+							Exquisite jewelry, made from shimmering gold and icy silver,
+							crafted with love and talent. Adorn your moments with the glamour
+							of LuxGleam. Accentuate your style with our curated collections
+							designed for every occasion. Click to explore and add a touch of
+							LuxGlam elegance to your life.
+						</p>
+						<div className={`${styles.ctgBottom} flex`}>
+							<SubCatCart
+								src={'/media/5/anklets.png'}
+								cat_name='Anklets'
+								color='silver'
+							/>
+							<SubCatCart
+								src={'/media/6/brooches.png'}
+								cat_name='Brooches'
+								color='gold'
+							/>
+						</div>
+					</div>
+
+					<SubCatCart
+						src={'/media/7/pendants.png'}
+						cat_name='Pendants'
+						color='gold'
+					/>
+				</div>
 			</section>
 			<section id='contact'>
 				<h2>Contact</h2>
