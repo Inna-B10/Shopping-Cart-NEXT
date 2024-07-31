3a7a94f850596b2c340b4fe676fe1080fcb8e7c9
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-07-31 03:22:04 +0200

    sortBy styling in progress

--- wwwroot/app/components/SortBy.jsx
+++ wwwroot/app/components/SortBy.jsx
@@ -1,19 +1,89 @@
+'use client'
+import { Prosto_One } from 'next/font/google'
+import { useState } from 'react'
 import styles from './SortBy.module.css'
 
+const prosto = Prosto_One({
+	weight: ['400'],
+	subsets: ['latin'],
+	variable: '--font-prosto',
+	display: 'swap',
+})
+
+/* 🠙  🠛   ▼  ▲  ˅  ˄  ⇩  ⇧  ᐁ  ᐃ*/
+const options = [
+	{ id: 'dateAZ', value: 'dateAZ', label: 'Date 🠙' },
+	{ id: 'dateZA', value: 'dateZA', label: 'Date 🠛' },
+	{ id: 'nameAZ', value: 'nameAZ', label: 'Name A-Z' },
+	{ id: 'nameZA', value: 'nameZA', label: 'Name Z-A' },
+	{ id: 'priceAZ', value: 'priceAZ', label: 'Price 🠙' },
+	{ id: 'priceZA', value: 'priceZA', label: 'Price 🠛' },
+]
+
 export default function SortBy() {
+	const [isExpanded, setIsExpanded] = useState(false)
+	const [selectedItem, setSelectedItem] = useState('')
+
+	const handleToggleChange = () => {
+		setIsExpanded(!isExpanded)
+		console.log(isExpanded)
+	}
+	const handleOptionChange = e => {
+		const selectedOption = options.find(
+			option => option.value === e.target.value
+		)
+		setSelectedItem(selectedOption)
+		setIsExpanded(false)
+	}
 	return (
-		<div className={styles.selectWrap}>
-			<select name='sortBy'>
-				<option value='select' disabled selected>
-					Sort by
-				</option>
-				<option value='nameAZ'>Name, A-Z</option>
-				<option value='nameZA'>Name, Z-A</option>
-				<option value='priceAZ'>Price, ↑</option>
-				<option value='priceZA'>Price, ↓</option>
-				<option value='dateAZ'>Date, ↑</option>
-				<option value='dateZA'>Date, ↓</option>
-			</select>
+		<div className={`${styles.select} ${prosto.variable}`}>
+			{/* <ul className={isExpanded ? styles.expanded : ''}>
+				<li className={styles.selectOption}>
+					<input type='radio' id='dateZA' value='dateZA' />
+					<label htmlFor='dateZA'>Date &#9661;</label>
+				</li>
+				<li className={styles.selectOption}>
+					<input type='radio' id='dateAZ' value='dateAZ' />
+					<label htmlFor='dateAZ'>Date &#9651;</label>
+				</li>
+				<li className={styles.selectOption}>
+					<input type='radio' id='nameZA' value='nameZA' />
+					<label htmlFor='nameZA'>Name Z-A</label>
+				</li>
+				<li className={styles.selectOption}>
+					<input type='radio' id='nameAZ' value='nameAZ' />
+					<label htmlFor='nameAZ'>Name A-Z</label>
+				</li>
+				<li className={styles.selectOption}>
+					<input type='radio' id='priceZA' value='priceZA' />
+					<label htmlFor='priceZA'>Price &#9661;</label>
+				</li>
+				<li className={styles.selectOption}>
+					<input type='radio' id='priceAZ' value='priceAZ' />
+					<label htmlFor='priceAZ'>Price &#9651;</label>
+				</li>
+			</ul> */}
+			<ul className={isExpanded ? styles.expanded : ''}>
+				{options.map(option => (
+					<li key={option.id} className={styles.selectOption}>
+						<input
+							type='radio'
+							id={option.id}
+							value={option.value}
+							name='sortOption'
+							onChange={handleOptionChange}
+						/>
+						<label htmlFor={option.id}>{option.label}</label>
+					</li>
+				))}
+			</ul>
+			<input
+				type='checkbox'
+				id='toggle'
+				className={styles.toggle}
+				onChange={handleToggleChange}
+			/>
+			<label htmlFor='toggle'>{selectedItem.label || 'Sort by'}</label>
 		</div>
 	)
 }
